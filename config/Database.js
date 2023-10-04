448ee02b789e3fe23376fa8af0f9cad0df9d43e3
verinarenata <renataverina@gmail.com>
2023-10-04 16:52:41 +0700

    install express dan nodemon

--- config/Database.js
+++ config/Database.js
@@ -1,18 +1,17 @@
-const Sequelize = require('sequelize');
-const mysql = require('mysql2');
+const Sequelize = require("sequelize");
+const mysql = require("mysql2");
 // create database connection
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'si-gercep'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "si-gercep",
 });
 db.connect((err) => {
-    if (err) {
-      console.error('Error connecting to MySQL:', err);
-    } else {
-      console.log('Connected to MySQL');
-    }
-  });
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+  } else {
+    console.log("Connected to MySQL");
+  }
+});
 module.exports = db;
-
